8fb35121cfc2e692c339243ed5ac7094c05768b7
Dastan Yeluybay <das.star.00@mail.ru>
2024-01-16 11:07:20 +0600

    Added new alliens, movement and shooting

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -19,12 +21,17 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         # Запуск основного цикла игры.
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -44,18 +51,69 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
-        # Реагирует на отпускание клавиш
+        # Реагирует на отпускание клавиш.
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        # Создание нового снаряда.
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # Обновляет позиции снарядов и уничтожает старые снаряды.
+        # Обновление позиций снарядов.
+        self.bullets.update()
+
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _create_fleet(self):
+        # Создание флота пришельцев.
+        # Создание пришельца т вычисление количества пришельцев в ряду
+        # Интервал между соседнимим пришельцами равен ширине пришельца.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        numbers_aliens_x = available_space_x // (2 * alien_width)
+
+        # Определяем количество рядов, помещающихся на экране.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Создание флота вторжения.
+        for row_number in range(number_rows):
+            for alien_number in range(numbers_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        # Создание пришельцев и размещение его в ряду.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2.4 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
     def _update_screen(self):
         # При каждом проходе цикла перерисовывается экран.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bulllet in self.bullets.sprites():
+            bulllet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
