083a412b30baa4df5b7eb1eee7c4b51ea4bc2a08
Dastan Yeluybay <das.star.00@mail.ru>
2024-01-21 01:57:17 +0600

    Add scoreboard, lifes, and high score, project is now Finished

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,8 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -24,12 +26,16 @@ class AlienInvasion:
 
         # Создание экземпляра для хранения игровой статистики
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Создание кнопки Play.
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         # Запуск основного цикла игры.
@@ -46,10 +52,36 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+    def _check_play_button(self, mouse_pos):
+        # Запускает игру при нажатии кнопки Play
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Сброс игровой статистики.
+            self.settings.initializw_dynamic_settings()
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
+
+            # Очистка списков пришельцев и снарядов
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Создание нового флота и размещение корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Указатель мыши скрывается.
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         # Реагирует на нажатие клавиш
@@ -90,12 +122,22 @@ class AlienInvasion:
     def _check_bullet_alien_collision(self):
         # Проверка попаданий в пришельцев.
         collisions = pygame.sprite.groupcollide(
-            self.bullets, self.aliens, True, True
-        )
+            self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
+            # Уничтожение снарядов, повышение скорости и создание нового флота.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         self._check_fleet_edges()
@@ -113,6 +155,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Уменьшение ships_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Очистка списков пришельцев и снарядов.
             self.aliens.empty()
@@ -126,6 +169,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         # Создание флота пришельцев.
@@ -184,6 +228,11 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        self.sb.show_score()
+
+        # Кнопка Play отображается в том случае, если игра не активна.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
--- game_stats.py
+++ game_stats.py
@@ -1,13 +1,16 @@
 class GameStats:
     # Отслеживает статистику для игры
     def __init__(self, ai_game):
+        self.high_score = 0
         # Инициализирует статистику
         self.settings = ai_game.settings
         self.reset_stats()
 
         # Игра запускается в активном состоянии.
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         # Инициализирует статистику, изменяющуюся в ходе игры
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
